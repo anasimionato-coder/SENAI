748a57073cf5ed45f4e1943fb39b0e0482af3649
ANA JÚLIA MOLINA SIMIONATO <ana_simionato@estudante.sesisenai.org.br>
2026-08-06 16:42:29 -0300

    teste

---
+++ Javascript/aula-09/exemplo-03/script.js
@@ -0,0 +1,10 @@
+const titulo = document.querySelector(".titulo")
+const btnAdicionar = document.querySelector(".btnAdicionar")
+const lista = document.querySelector(".lista")
+btnAdicionar.addEventListener("click",function(){
+    const li = document.createElement("li")
+    li.textContent = "A anny, Adicionou mais um item"
+    lista.appendChild(li)
+
+})
+ 
