027a052410b0fffe5d28f888afe5aa20ad19fa4d
ANA JÚLIA MOLINA SIMIONATO <ana_simionato@estudante.sesisenai.org.br>
2026-08-06 17:12:53 -0300

    teste

---
+++ Javascript/aula-09/exemplo-05/script.js
@@ -0,0 +1,11 @@
+const botao = document.querySelector("btnMostrar");
+
+const paragrafo = document.querySelector("resultado");
+
+botao.addEventListener("click",function(){
+    
+const titulo = document.querySelector("h1");
+
+resultado.textContent = titulo.textContent
+
+})
